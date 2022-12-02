ec67a18616b70b8ec43ff0e0c1e574850915ae9b
RJ <github@daweb.site>
2022-12-02 00:29:46 -0600

    Shortened slightly more

--- day1.py
+++ day1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 if __name__ == '__main__':
-    sizes=[]
-    with open('day1input.txt','r') as f: sizes = [sum([int(y) for y in x.split('\n') if y != '']) for x in f.read().split('\n\n')]
-    sizes.sort()
-    print(sizes[-1])
-    print(sizes[-3]+sizes[-2]+sizes[-1])
+    with open('day1input.txt','r') as f: 
+        sizes = [sum([int(y) for y in x.split('\n') if y != '']) for x in f.read().split('\n\n')]
+        sizes.sort()
+        print(sizes[-1])
+        print(sizes[-3]+sizes[-2]+sizes[-1])
