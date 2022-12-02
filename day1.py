baf3c45916a51208e8146b437063a9a0366e1046
RJ <github@daweb.site>
2022-12-02 00:31:43 -0600

    And one more time for the hell of it.

--- day1.py
+++ day1.py
@@ -3,5 +3,4 @@ if __name__ == '__main__':
     with open('day1input.txt','r') as f: 
         sizes = [sum([int(y) for y in x.split('\n') if y != '']) for x in f.read().split('\n\n')]
         sizes.sort()
-        print(sizes[-1])
-        print(sizes[-3]+sizes[-2]+sizes[-1])
+        print(f'P1:{sizes[-1]}-P2:{sizes[-3]+sizes[-2]+sizes[-1]}')
