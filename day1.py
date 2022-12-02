f4683905785d34e5c2d12209ba2335f441e6efb8
RJ <github@daweb.site>
2022-12-02 00:28:19 -0600

    Shortened

--- day1.py
+++ day1.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python3
-
-
 if __name__ == '__main__':
-    sizes: list[int] = []
-    cur = 0
-    with open('day1input.txt','r') as f:
-        for line in f:
-            line = line.strip()
-            if line == '':
-                sizes.append(cur)
-                cur = 0
-            else:
-                cur += int(line.strip())
+    sizes=[]
+    with open('day1input.txt','r') as f: sizes = [sum([int(y) for y in x.split('\n') if y != '']) for x in f.read().split('\n\n')]
     sizes.sort()
-    print(sizes)
+    print(sizes[-1])
     print(sizes[-3]+sizes[-2]+sizes[-1])
